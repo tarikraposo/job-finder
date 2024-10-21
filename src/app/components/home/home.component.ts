a4926a52d72291e67faaa21191f1e0d42024b743
Tarik Raposo Parreira <tarik.parreira@mpdft.mp.br>
2024-10-21 14:03:48 -0300

    Feat: adding card

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,36 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatSelectModule } from '@angular/material/select'
 import { MatInputModule } from '@angular/material/input'
 import { HomeService } from '../../services/home.service';
 
-interface JobProps {
-  id: number,
-  name: string,
-  website?: string,
-  linkedin?: string,
-  github?: string,
-  title: string,
-  country: string,
-  city: string,
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 
-}
+
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatSelectModule, MatInputModule],
+  imports: [FormsModule, MatFormFieldModule, MatSelectModule, MatInputModule, MatProgressSpinnerModule, MatCardModule, CommonModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent<JobProps> {
+  isLoading = true;
+  data: any = ''
+
+  @Input() title = '';
+  @Input() location = '';
+  @Input() description = '';
+  @Input() company = '';
+  @Input() website_url = '';
 
   constructor(private service: HomeService) {
     this.service.getCountry().subscribe(
-      (data: any) => {
-        console.log(data)
+      (Data: any) => {
+
+        this.data = Data;
       })
+    this.isLoading = false;
   }
 
 }
--- src/app/services/home.service.ts
+++ src/app/services/home.service.ts
@@ -2,19 +2,29 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+interface JobProps {
+  title: string,
+  location: string,
+  description: string,
+  company: string,
+  website_url: string,
+}
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
 
-  apiUrl = "https://jobdataapi.com/api/jobs/?country_code=BR"
+  apiUrl = "https://jobdataapi.com/api/jobs/?country_code=BR";
+  // 
 
   constructor(private client: HttpClient) {
 
   }
 
   getCountry(): Observable<any> {
-    return this.client.get(this.apiUrl)
+    return this.client.get<JobProps>(this.apiUrl)
   }
 
 }
